6e478a8dd2dccd557274084319e8c4aeb5890f0a
Christian von Essen <christian@mvonessen.de>
2011-02-25 15:58:50 +0100

    randBG.lisp: Make a daemon that contously updates the background image.

--- randBG.lisp
+++ randBG.lisp
@@ -13,9 +13,18 @@
 
 (defparameter *bg-dir* (merge-pathnames #P"backgrounds/" *home-dir*))
 
+(defun path-/ (directory &rest names)
+  (declare (inline path-/)
+           (type (or (cons string) (cons pathname)) names)
+           (type (or string pathname) directory))
+  (loop for name in names
+        for acc = (cl-fad:pathname-as-directory directory) then (cl-fad:pathname-as-directory acc)
+        do (setf acc (merge-pathnames acc name))
+        finally (return acc)))
+
+
 (defun ls (path)
-  (let ((dir (sb-posix:opendir path))
-        (dirpath (path name-as-directory path)))
+  (let ((dir (sb-posix:opendir path)))
     (unwind-protect
          (loop
             :for  entry = (sb-posix:readdir dir)
@@ -26,8 +35,8 @@
       (sb-posix:closedir dir))))
 
 (defparameter *resolutions*
-  (mapcar (lambda (d) (pathname-as-directory
-                       (merge-pathnames d *bg-dir*))) '("1440x900" "1280x1024")))
+  (mapcar (lambda (res) (path-/ *bg-dir* res))
+          '("1680x1050")))
 
 (defun random-elt (list)
   (unless (and (listp list) (not (null list))) (error "list is not a list"))
@@ -45,7 +54,7 @@
          (values ,output ,exit-code)))))
 
 (defun set-bg (path)
-  (or-raise [ habak -mp 0,0 -hi ?path ]))
+  [ habak -mp 0,0 -hi ?path ])
 
 (defun append-images (&rest paths)
   (let ((pathname (sl:mktemp)))
@@ -53,11 +62,21 @@
     pathname))
 
 (defun main (args)
-  (declare (ignore args))
+  (pop args)
   (let* ((*random-state* (make-random-state t))
          (imgs
-          (mapcar (lambda (dir) (merge-pathnames (random-elt (ls dir)) dir))
-                  *resolutions*))
-         (merged (apply #'append-images imgs)))
-    (set-bg merged)
-    (delete-file merged)))
+          (mapcar #'ls *resolutions*)))
+    (labels ((random-imgs ()
+               (mapcar (lambda (imgs res) (path-/  res (random-elt imgs))) imgs *resolutions*))
+             (set-bgs ()
+               (let ((merged (apply #'append-images (random-imgs))))
+                 (set-bg merged)
+                 (delete-file merged))))
+      (if (null args)
+          (set-bgs)
+          (let ((interval (parse-integer (car args) :junk-allowed t)))
+            (when (null interval)
+              (format *error-output* "supplied argument ~s is not a number~%" (car args))
+              (sb-ext:quit :unix-status 1))
+            (loop do (set-bgs) do (sleep interval)))))))
+                
